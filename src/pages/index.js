2617469380c10fa0fdbeaf31d382f3c2a1477229
Nate Siggard <48007753+natesiggard@users.noreply.github.com>
2023-05-02 20:50:22 -0700

    Updated page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { NOTION_DATABASE_ID } from '../config'; // Import the constant from conf
 import Link from 'next/link';
 import styles from './newsItem.module.css'; // Import the CSS module
 
-
 // Helper function to format date and time
 const formatDateAndTime = (dateString) => {
   const options = {
@@ -54,46 +53,75 @@ export async function getServerSideProps(context) {
 
 export default function Home({ results }) {
   const router = useRouter();
+  const selectedTag = router.query.tag; // Get the selected tag from the query parameter
+
+  // Extract all unique tags from the results
+  const uniqueTags = new Set();
+  results.forEach((result) => {
+    result.tags.forEach((tag) => uniqueTags.add(tag));
+  });
 
   return (
     <div>
       <h1>AI News</h1>
 
-       {/* Add a link to reset filters and show all results */}
-      <div className="showAll">
-        <Link href="/">
-          Show All
-        </Link>
-      </div>
-
-      {results.map((result, index) => (
-        <div key={index} className={styles.newsItem}>
-          <div className="bmDate">{result.bookmarkedDate}</div>
-          <div>{result.name}</div>
-          <div>
-            {result.tags.map((tag, tagIndex) => (
-              <span key={tagIndex}>
-                <a
-                  href="#"
-                  onClick={(e) => {
-                    e.preventDefault();
-                    router.push(`/?tag=${encodeURIComponent(tag)}`);
-                  }}
-                >
-                  {tag}
-                </a>
-                {tagIndex < result.tags.length - 1 && ', '}
-              </span>
-            ))}
-          </div>
-          <div>
-            <a href={result.url} target="_blank" rel="noopener noreferrer">
-              Read More
-            </a>
-          </div>
-          <div>Published: {result.publishedDate}</div>
+      {/* Conditionally render the list of all unique tags as links based on the absence of the selectedTag */}
+      {!selectedTag && (
+        <div className="tagsList">
+          {Array.from(uniqueTags).map((tag, index) => (
+            <span key={index}>
+              <a
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault();
+                  router.push(`/?tag=${encodeURIComponent(tag)}`);
+                }}
+              >
+                {tag}
+              </a>
+              {index < uniqueTags.size - 1 && ', '}
+            </span>
+          ))}
         </div>
-      ))}
+      )}
+
+      {/* Conditionally render the "Show All" link based on the presence of the selectedTag */}
+      {selectedTag && (
+        <div className="showAll">
+          <Link href="/">
+            Show All
+          </Link>
+        </div>
+      )}
+        {results.map((result, index) => (
+    <div key={index} className={styles.newsItem}>
+      <div className="bmDate">{result.bookmarkedDate}</div>
+      <div>{result.name}</div>
+      <div>
+        {result.tags.map((tag, tagIndex) => (
+          <span key={tagIndex}>
+            <a
+              href="#"
+              onClick={(e) => {
+                e.preventDefault();
+                router.push(`/?tag=${encodeURIComponent(tag)}`);
+              }}
+            >
+              {tag}
+            </a>
+            {tagIndex < result.tags.length - 1 && ', '}
+          </span>
+        ))}
+      </div>
+      <div>
+        <a href={result.url} target="_blank" rel="noopener noreferrer">
+          Read More
+        </a>
+      </div>
+      <div>Published: {result.publishedDate}</div>
     </div>
-  );
+  ))}
+</div>
+);
 }
+           
